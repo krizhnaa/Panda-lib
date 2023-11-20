f996e68358a4ea0a20acdf3817b6ee5ffa302e17
krizhnaa <krizhnaprem@gmail.com>
2023-11-20 20:30:06 +0530

    1st Obj done

---
+++ README.md
@@ -0,0 +1,3 @@
+// Creating a US States game with the help of panda library
+
+# <1> Fetching the US state image and displaying it as the main screen.
---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+from turtle import Screen
+
+screen = Screen()
+image = 'blank_states_img.gif'
+
+screen.title("US States Game")
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+screen.exitonclick()
