43fc4e603254099f77a68d9db75dc3e2c455cc0d
krizhnaa <krizhnaprem@gmail.com>
2023-11-20 22:47:53 +0530

    Done with the main project

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ turtle.shape(image)
 
 s_turtle = turtle.Turtle()
 
+correct_guess = []
+
 is_game_on = True
+count = 0
 
 while is_game_on:
-    answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name? ").title()
+    if count == 0:
+        answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name? ").title()
+    else:
+        answer_state = screen.textinput(title=f"{count_correct}/50", prompt="What's another state's name? ").title()
     df = pa.read_csv('50_states.csv')
     x_loc = int(df[df['state'] == answer_state]['x'])
     y_loc = int(df[df['state'] == answer_state]['y'])
@@ -22,14 +28,13 @@ while is_game_on:
     states_list = states.tolist()
     for state in states_list:
         if answer_state == state:
-            print("Damnnnnn")
             s_turtle.penup()
             s_turtle.hideturtle()
             s_turtle.goto(x_loc, y_loc)
             s_turtle.write(f"{answer_state}", align='center')
+            correct_guess.append(answer_state)
+            count_correct = len(correct_guess)
 
-
-
-
+    count = 1
 
 turtle.mainloop()
